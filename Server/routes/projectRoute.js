156933d94ec0efc1b29e788d0599d7ed4b737f36
Ponnarasu <ponnarasua22it@srishakthi.ac.in>
2024-09-25 10:33:24 +0530

    projectroute

--- Server/routes/projectRoute.js
+++ Server/routes/projectRoute.js
@@ -1,26 +1,56 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
-const project = require('../models/projectModel');
+const Projects = require('../models/projectModel')
 
 router.get('/all', async (req, res) => {
     try {
-        const fetchedproject = await project.find();
-        res.json(fetchedproject).status(200);
+        const fetchedprojects = await Projects.find()
+        res.status(200).json(fetchedprojects)
     } catch (error) {
-        res.json(error).status(200);
+        res.status(500).json(error)
     }
-});
+})
 
 router.post('/add', async (req, res) => {
     try {
-        const newproject = await new project(req.body);
+        const newprojectdata = new Projects(req.body)
         const { title, desc } = newprojectdata
-        if(!title || desc){
-            res.json({ message: "Title & Desc Required" }).status(500)
+        if (!title || !desc) {
+            res.status(400).json({ message: "Title & Desc Required" })
+
         }
         const savedata = await newprojectdata.save()
-        res.json(savedata).status(201);
+        res.status(201).json(savedata)
+
     } catch (error) {
-        res.json(error).status(500);
+        res.status(500).json(error)
     }
 })
+router.put('/edit/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const currentrecord = await Projects.findOne({ _id: id })
+        if (!currentrecord) {
+            res.status(404).json({ message: "Project not found !" })
+        }
+        const updateProject = await Projects.findByIdAndUpdate(id, req.body, { new: true })
+        res.status(200).json(updateProject)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const currentrecord = await Projects.findOne({ _id: id })
+        if (!currentrecord) {
+            res.status(404).json({ message: "Project not found !" })
+        }
+        const deleteProject = await Projects.findByIdAndDelete(id)
+        res.status(200).json({ message: "Project Deleted !" })
+    } catch (e) {
+        res.status(500).json(error)
+    }
+})
+
+module.exports = router
--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const dbConn = require('./config/db.js')
 const project = require("./routes/projectRoute.js")
-require('dotenv').config();
-const port = process.env.PORT || 2005
+const port = process.env.PORT;
 app.use(express.json())
 app.get('/', (req, res) => {
     res.json("Welcome").status(400);
